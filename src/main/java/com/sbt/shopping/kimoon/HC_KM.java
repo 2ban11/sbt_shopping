b90426429c6ac8f74f4820b0605a228f27830125
2ban11 <2ban11@naver.com>
2023-08-08 17:40:59 +0900

    add MainPageContents

--- src/main/java/com/sbt/shopping/kimoon/HC_KM.java
+++ src/main/java/com/sbt/shopping/kimoon/HC_KM.java
@@ -4,8 +4,11 @@ import java.text.DateFormat;
 import java.util.Date;
 import java.util.Locale;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,10 +18,13 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @Controller
 public class HC_KM {
 	
+@Autowired
+private MainPageDAO mpDAO;
 	
 	@RequestMapping(value = "/", method = RequestMethod.GET)
-	public String mainPage(Locale locale, Model model) {
-		
+	public String mainPage(HttpServletRequest request) {
+		// mpDAO.get48Products(request);
+		request.setAttribute("contentPage", "kimoon/kimoonMainPage.jsp");
 		return "index";
 	}
 	
