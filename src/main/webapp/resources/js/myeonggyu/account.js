0f10e39837b51fb1d6850ee9c5e1a7232a997cee
pmg782 <mic72826@gmail.com>
2023-08-30 17:28:33 +0900

    8/30

--- src/main/webapp/resources/js/myeonggyu/account.js
+++ src/main/webapp/resources/js/myeonggyu/account.js
@@ -12,15 +12,35 @@ $(document).ready(function() {
             $("#a_email2").val($(this).val()).prop("disabled", true);
         }
     });
+	
+	function validateId() {
+    var a_id = $("#a_id").val();
+    var idRegex = /^(?=.*[a-zA-Z])(?=.*[0-9])[a-zA-Z0-9]{1,12}$/; // 영어와 숫자만, 18자 이하
 
+    if (!idRegex.test(a_id)) {
+        alert("아이디는 영어와 숫자로 12자 이하로 입력해주세요.");
+        return false;
+    }
+
+    return true;
+}
+	
     // 이메일 유효성 검사
-    function validateEmail() {
+  function validateEmail() {
     var emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
     var a_email3 = $("#a_email3").val();
     var a_email2 = $("#a_email2").val(); // 직접 입력한 이메일 도메인 값 가져오기
 
     if (a_email3 == 'direct') {
         var a_id = $("#a_id").val(); // 직접 입력한 아이디 값 가져오기
+
+        // 아이디 값이 영어와 숫자 이외의 문자를 포함하는지 확인
+        var idRegex = /^[a-zA-Z0-9]+$/;
+        if (!idRegex.test(a_id)) {
+            alert("아이디는 영어와 숫자로만 입력해주세요.");
+            return false;
+        }
+
         if (a_id == "" || a_email2 == "" || !emailRegex.test(a_id + "@" + a_email2)) {
             alert("올바른 이메일 주소를 입력하세요.");
             return false;
@@ -98,12 +118,13 @@ function isPhoneNumberDuplicated() {
     // 회원가입 폼 제출을 처리하는 함수
  $(".joinForm").submit(function(event) {
     event.preventDefault(); // 폼 제출 막음
-    
+
     var isEmailValid = validateEmail();
     var isNicknameValid = validateNickname();
     var isPasswordValid = validatePassword();
     var isPasswordMatched = checkPasswordMatch();
-    var phoneNumberIsDuplicated = isPhoneNumberDuplicated(); // 변수명 변경
+    var phoneNumberIsDuplicated = isPhoneNumberDuplicated();
+    var isIdValid = validateId(); // 아이디 유효성 검사
 
     // 아이디 중복 확인
     if (isIdDuplicated()) {
@@ -117,9 +138,9 @@ function isPhoneNumberDuplicated() {
     }
 
     // 유효성 검사 실패 시 폼 제출 막음
-    if (!isEmailValid || !isNicknameValid || !isPasswordValid || !isPasswordMatched || phoneNumberIsDuplicated) {
+    if (!isEmailValid || !isNicknameValid || !isPasswordValid || !isPasswordMatched || phoneNumberIsDuplicated || !isIdValid) {
         alert("입력한 정보를 다시 확인해주세요.");
-        return false; // 유효성 검사 실패 시 폼 제출 막음
+        return false;
     }
 
     // 모든 유효성 검사를 통과하면 폼을 제출합니다.
@@ -131,15 +152,22 @@ function isPhoneNumberDuplicated() {
 $("#btn_id_chk").click(function() {
     var a_id = $("#a_id").val();
     var a_email2 = $("#a_email2").val();
-    
+
+    // 아이디 값이 숫자와 영어가 합쳐진 값인지 확인
+    var idRegex = /^(?=.*[a-zA-Z])(?=.*[0-9])[a-zA-Z0-9]+$/;
+    if (!idRegex.test(a_id)) {
+        alert("아이디는 영어와 숫자가 합쳐진 값으로 입력해주세요.");
+        return;
+    }
+
     if (a_id === "") {
         alert("아이디를 입력해주세요");
         return;
     } else if (a_email2 === "") {
-        alert("주소를 선택해주세요");
+        alert("이메일 주소를 선택해주세요");
         return;
     }
-    
+
     var domain = a_email2.substring(a_email2.indexOf('@') + 1); // 도메인 추출
     
     // AJAX를 사용하여 MX 레코드 확인을 요청합니다.
@@ -175,34 +203,7 @@ $("#btn_id_chk").click(function() {
             alert("MX 레코드 확인 중 오류가 발생했습니다. 다시 시도해주세요.");
         }
     });
-}); 
-
-    // 닉네임 중복 확인 버튼 클릭 시
-    $("#btn_nickname_chk").click(function() {
-        var a_nickname = $("#a_nickname").val();
-        if (a_nickname === "") {
-            alert("닉네임을 입력해주세요");
-            return;
-        }
-        // AJAX를 사용하여 닉네임 중복 확인을 요청합니다.
-        $.ajax({
-            url: "/shopping/accountNickname.get",
-            type: "GET",
-            data: { a_nickname: a_nickname },
-            success: function(result) {
-                if (result === 1) {
-                    alert("이미 사용 중인 닉네임입니다.");
-                    $("#nickname_chk").val("");
-                } else {
-                    alert("사용 가능한 닉네임입니다.");
-                    $("#nickname_chk").val("1");
-                }
-            },
-            error: function(xhr, status, error) {
-                alert("닉네임 중복 확인 중 오류가 발생했습니다. 다시 시도해주세요.");
-            }
-        });
-    });
+});
 $("#btn_phone_chk").click(function() {
     var a_phone = $("#a_phone").val();
 
